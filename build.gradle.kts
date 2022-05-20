e186d5a015f0823594758255af42de0b89d14a60
Matt Halstead <mhalstead0@gmail.com>
2022-05-20 19:49:00 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.21"
+}
+
+group = "com.matthalstead"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
